4e845adbb383ef296391d618587caee9a8ba046c
vengidusamim-star <vengidusamim@gmail.com>
2026-01-26 14:43:45 +0530

    Update CertifiedVideoCourses.tsx

--- src/pages/CertifiedVideoCourses.tsx
+++ src/pages/CertifiedVideoCourses.tsx
@@ -60,7 +60,7 @@ const videoCourses: VideoCourse[] = [
     category: "Data Structures",
     certified: true,
   },
-    {
+ {
     id: "Harvard-University",
     title: "CS50x2026-Lecture0-Scratch",
     description: "This is CS50, Harvard University's introduction to the intellectual enterprises of computer science and the art of programming.",
@@ -70,6 +70,47 @@ const videoCourses: VideoCourse[] = [
     category: "cs50",
     certified: true,
   },
+  {
+    id: "Harvard-University",
+    title: "CS50x2026-Lecture1-C",
+    description: "This is CS50, Harvard University's introduction to the intellectual enterprises of computer science and the art of programming.",
+    youtubeId: "SlqjA04_dpk",
+    duration: "Week 1 Course",
+    level: "Intermediate",
+    category: "cs50",
+    certified: true,
+  },
+  {
+    id: "Harvard-University",
+    title: "CS50x2026-Lecture2-Arrays",
+    description: "This is CS50, Harvard University's introduction to the intellectual enterprises of computer science and the art of programming.",
+    youtubeId: "h5Gc1n8ZuU8",
+    duration: "Week 2 Course",
+    level: "Intermediate",
+    category: "cs50",
+    certified: true,
+  },
+  {
+    id: "Harvard-University",
+    title: "CS50x2026-Lecture3-Algorithms",
+    description: "This is CS50, Harvard University's introduction to the intellectual enterprises of computer science and the art of programming.",
+    youtubeId: "6Svu_ae5ebk",
+    duration: "Week 3 Course",
+    level: "Intermediate",
+    category: "cs50",
+    certified: true,
+  },
+   {
+    id: "Harvard-University",
+    title: "CS50x2026-Lecture4-Memory",
+    description: "This is CS50, Harvard University's introduction to the intellectual enterprises of computer science and the art of programming.",
+    youtubeId: "db0H0U13YsA",
+    duration: "Week 3 Course",
+    level: "Intermediate",
+    category: "cs50",
+    certified: true,
+  },
+  
 
 ];
 
